eba1a0ee234582fbcfad36c5dc2f56ca507e0cbd
Supphakorn021 <supphakorn.praisakuldecha@mail.kmutt.ac.th>
2023-12-10 17:59:21 +0700

    feat: add report by time

--- route/reportRoute.js
+++ route/reportRoute.js
@@ -1,29 +1,25 @@
-const express = require('express'); // Import express
-const router = express.Router();  // Create a new router
-const reportController = require("../controller/reportController") // Import reportController
-
-
-
-// router.get('/report', async (req, res) => {
-//   try {
-//     const result = await reportController.getReports();
-//     res.status(200).json(result);
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(500).json({ message: error.message });
-//   }
-// });
+const express = require('express') // Import express
+const router = express.Router() // Create a new router
+const reportController = require('../controller/reportController') // Import reportController
 
 router.get('/', async (req, res) => {
   try {
-    const reports = await reportController.getReports();
-    res.status(200).json(reports);;
+    const reports = await reportController.getReports()
+    res.status(200).json(reports)
   } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ message: error.message });
+    console.error(error.message)
+    res.status(500).json({ message: error.message })
   }
-});
+})
 
-
+router.get('/unique', async (req, res) => {
+  try {
+    const reports = await reportController.getUniqueReports()
+    res.status(200).json(reports)
+  } catch (error) {
+    console.error(error.message)
+    res.status(500).json({ message: error.message })
+  }
+})
 
 module.exports = router
