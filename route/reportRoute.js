8f28b36d6245347089d6963c20a2e3255aa91299
Supphakorn021 <supphakorn.praisakuldecha@mail.kmutt.ac.th>
2023-12-11 04:25:02 +0700

    fix : Fixed rainStatus can be able to select multiple.

--- route/reportRoute.js
+++ route/reportRoute.js
@@ -6,16 +6,19 @@ router.get('/', async (req, res) => {
   try {
     const { specificTime, sort, rainStatus } = req.query;
     let reports;
+
     if (specificTime) {
       reports = await reportController.getReportsBySpecificTime(specificTime);
     } else {
       reports = await reportController.getReports();
     }
     if (rainStatus) {
-      const rainStatusArray = rainStatus.split(',');
-      let filteredReports = reports;
-      filteredReports = reports.filter(report => rainStatusArray.includes(report.rainStatus));
-      reports = filteredReports;
+      if (rainStatus.toLowerCase() !== 'all') {
+        const rainStatusArray = rainStatus.split(',');
+        let filteredReports = reports;
+        filteredReports = reports.filter(report => rainStatusArray.includes(report.rainStatus));
+        reports = filteredReports;
+      }
     }
     if (sort) {
       const [sortBy, sortOrder] = sort.split(',');
