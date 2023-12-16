d37063fb55816c360d097a24ed0ee4942caaf6c9
thunnavat <thunnavat.c@mail.kmutt.ac.th>
2023-12-16 20:17:50 +0700

    fix: fix code design

--- models/district.js
+++ models/district.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose')
 
-const districtSchema = new mongoose.Schema({
-  _id: { type: Number, required: true, primary: true },
-  districtName: { type: String, required: true, unique: true },
-  coords: { type: Array, required: true },
-}, { versionKey: false })
-
+const districtSchema = new mongoose.Schema(
+  {
+    _id: { type: Number, required: true, primary: true },
+    districtName: { type: String, required: true, unique: true },
+    coords: { type: Array, required: true },
+  },
+  { versionKey: false }
+)
 
 const District = mongoose.model('District', districtSchema)
 
-
 module.exports = District // Export District model
-
-
--- route/districtRoute.js
+++ route/districtRoute.js
@@ -1,27 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const districtController = require('../controller/districtController'); //
+const districtController = require('../controller/districtController') //
 
-//routes
-
-router.get('/', async (req, res) => { // Get all districts
-  try {
-    const result = await districtController.getDistricts();
-    res.status(200).json(result);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
-
-router.post('/', async (req, res) => {
-  try {
-    const result = await districtController.addDistricts(req.body);
-    res.status(201).json(result);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ message: error.message });
-  }
-});
+router.get('/', districtController.getDistricts)
+router.post('/', districtController.addDistricts)
 
 module.exports = router
